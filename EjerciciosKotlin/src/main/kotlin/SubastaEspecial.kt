a617f1894a8ebf2fbe6468690e69bd631409307c
wendelledgardossantoschabaud <wendelledgardossantoschabaud@gmail.com>
2023-09-29 20:03:19 +0200

    "agregado return mais eficiente"

--- EjerciciosKotlin/src/main/kotlin/SubastaEspecial.kt
+++ EjerciciosKotlin/src/main/kotlin/SubastaEspecial.kt
@@ -4,10 +4,13 @@ fun main() {
 
     println("Item A is sold at ${auctionPrice(winningBid, 2000)}.")
     println("Item B is sold at ${auctionPrice(null, 3000)}.")
+
 }
 
 class Bid(val amount: Int, val bidder: String)
 
 fun auctionPrice(bid: Bid?, minimumPrice: Int): Int {
     //if(bid!=null && bid.amount>0) return bid.amount else return minimumPrice
-    return bid?.let{} ?: minimumPrice}
+    //return bid?.let{if(it.amount<0) minimumPrice else it.amount} ?: minimumPrice
+    return bid?.takeIf{it.amount > 0}?.amount ?: minimumPrice
+}
